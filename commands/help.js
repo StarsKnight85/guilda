e6033723d66f5cd349f4e791a23cd9458cdd5a70
BrigitteLPB <57198764+BrigitteLPB@users.noreply.github.com>
2020-01-15 19:38:53 +0100

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'help',
-    description: "help command",
+    description: "help command. You can have help with \"help <command>\"",
     permission: "all",
     execute(guilda, message, args){
         //création de l'embed
@@ -10,14 +10,12 @@ module.exports = {
             for (command of commands){
                 const guildaCommand = guilda.commands.get(command)
                 const txt = `>>help-  ${guildaCommand.name}: ${guildaCommand.description} <permission:${guildaCommand.permission}>`
-                console.log(txt)
                 message.channel.send(txt)
             }
             //console.log(command.name + ":" + command.description)//debug
                 //embed with command
         }else{
             txt = `>>help-  ${guilda.commands.get(args[1]).name}: ${guilda.commands.get(args[1]).description}  <permission:${guilda.commands.get(args[1]).permission}>`
-            console.log(txt)
             message.channel.send(txt)
         }
     }
